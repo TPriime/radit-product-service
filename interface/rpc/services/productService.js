6e2f318327ff64a50084ecbb0354f9d90cd30dbd
Anthony O. Agbafune <tonypriime@gmail.com>
2021-07-27 15:34:06 +0100

    fix(productService): wrapped product id in mongo object

--- interface/rpc/services/productService.js
+++ interface/rpc/services/productService.js
@@ -1,55 +1,56 @@
 const loader = require("../utils/loader");
 const Product = require("../../commons/models/product");
 const PROTO_PATH = __dirname + '/../proto/product.proto';
+const { ObjectID } = require('mongodb');
 
 const protoDescriptor = loader.loadProto(PROTO_PATH);
 
 function getProductByName(call, callback) {
-    const productName = call.request.productName
-    Product.findOne(
-        {
-          productName: productName
-        },
-        null,
-        (error, product) => {
-          const status = !error && product;
-          // set productId key for response
-          if(product) product.productId = product._id.toString();
-          return callback(error, { status, product });
-        }
-      );
-    // callback(null, {
-    //     status: true,
-    //     product: { productName: "seriveYam", productPrice: 160 },
-    // });
+  const productName = call.request.productName
+  Product.findOne(
+    {
+      productName: productName
+    },
+    null,
+    (error, product) => {
+      const status = !error && product;
+      // set productId key for response
+      if (product) product.productId = product._id.toString();
+      return callback(error, { status, product });
+    }
+  );
+  // callback(null, {
+  //     status: true,
+  //     product: { productName: "seriveYam", productPrice: 160 },
+  // });
 }
 
 
 function getProductById(call, callback) {
-    const productId = call.request.productId
-    // return callback(null, {
-    //     status: true,
-    //     product: { productId: call.request.productId, productName: "seriveYam", productPrice: 160 },
-    // });
+  try {
+    const productId = ObjectID(call.request.productId)
     Product.findOne(
-        {
-          _id: productId
-        },
-        null,
-        (error, product) => {
-          const status = !error && product;
-          // set productId key for response
-          if(product) product.productId = product._id.toString();
-          return callback(error, { status, product });
-        }
-      );
+      {
+        _id: productId
+      },
+      null,
+      (error, product) => {
+        const status = !error && product;
+        // set productId key for response
+        if (product) product.productId = product._id.toString();
+        return callback(error, { status, product });
+      }
+    );
+  } catch (err) {
+    return callback(err, { status: false, product: null });
+  }
 }
 
 
 module.exports = {
-    service: protoDescriptor.ProductService.service,
-    methods: {
-        getProductById,
-        getProductByName
-    }
+  service: protoDescriptor.ProductService.service,
+  methods: {
+    getProductById,
+    getProductByName
+  }
 };
